03eb247846a8e0482786f0ef42aaaf3ba3b0696f
Marcone-Santo1 <marconemagalhaes203@gmail.com>
2022-11-23 17:44:05 -0300

    add js

---
+++ app/views/js/index.js
@@ -0,0 +1,12 @@
+var fcnListarTemplates = (function () {
+    $.ajax({
+        url: "./views/listar.php",
+        type: 'get',
+        dataType: 'html'
+    })
+        .done(function (msg) {
+            $("#tblista").html(msg);
+        })
+});
+
+fcnListarTemplates();
