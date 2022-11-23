2d70a7383a7bd7227eced1dd46a408a36456e613
Marcone-Santo1 <marconemagalhaes203@gmail.com>
2022-11-23 17:56:35 -0300

    add script index.js

---
+++ app/views/script/index.js
@@ -0,0 +1,14 @@
+var fcnListar = (function () {
+    $.ajax({
+        url: ".//views//listar.php",
+        type: 'get',
+        dataType: 'html'
+    })
+        .done(function (msg) {
+            $("#tblista").html(msg);
+        })
+});
+
+$(function () {
+    fcnListar();
+});
